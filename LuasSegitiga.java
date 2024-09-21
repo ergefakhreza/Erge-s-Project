43326a73576319ddc3020bdc4f12c4cf0ea7f611
ergefakhreza <ergefakhreza@gmail.com>
2024-09-21 13:42:36 +0800

    Add files via upload

---
+++ LuasSegitiga.java
@@ -0,0 +1,11 @@
+package tipedata;
+
+
+public class LuasSegitiga {
+    public static void main(String[] args) {
+        int alas = 7;
+        int tinggi = 3;
+        double luasSegitiga = alas*tinggi*0.5;
+        System.out.println("Luas Segitiga : " + luasSegitiga);
+    }
+}
